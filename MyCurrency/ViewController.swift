7b9ca7d9fd5a348a46c6379c6edb915eef142962
danang swastiko <danang.sakti@sehatQ.com>
2019-11-18 13:07:51 +0700

    Initial Commit

---
+++ MyCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCurrency
+//
+//  Created by danang sakti on 18/11/19.
+//  Copyright © 2019 arclaire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
